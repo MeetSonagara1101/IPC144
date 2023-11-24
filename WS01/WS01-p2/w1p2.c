48c16feb5f5f258abb3f93b846273ba23e9d64d6
Meet HiteshBhai Sonagara <151964365+MeetSonagara1101@users.noreply.github.com>
2023-11-24 15:09:25 -0500

    Workshop-1 Solution
    Introduction to Programming using c workshop-1

---
+++ WS01/WS01-p2/w1p2.c
@@ -0,0 +1,34 @@
+/*
+*****************************************************************************
+<assessment name example : Workshop - #1 (Part - 2)>
+Full Name :
+Student ID# :
+	Email :
+	Section :
+	Authenticity Declaration :
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider.This submitted
+piece of work is entirely of my own creation.
+******************************************************************************
+*/
+
+//For More Contact me on instagram or mail
+//Instagram: @meetsonagara1101
+//Mail : connect.meetsonagara1101@gmail.com
+
+#include <stdio.h>
+int main() {
+	printf("Workshop 1 Part-2\n=================\n\n");
+	printf("Using tab specifiers...\n");
+	printf("\tTab-1\tTab-2\tTab-3\n\t-----\t=====\t-----\n\n");
+	printf(
+		"\\ this is a back-slash character!\n"
+		"%% this is a percentage sign character!\n"
+		"\" this is  a double-quote character\n\n"
+	);
+	printf("My favourite quotes are:\n");
+	printf("1.\t\"Skill is Only developed by hours and hours of work.\"\n\t\t\t\t\t\t-Usain Bolt\n");
+	printf("2.\t\"It's not about having time. It's about making time\"\n\t\t\t\t\t\t-unknown\n");
+	printf("3.\t\"All of us do not have equal talant. But, all of us\n\t have an equal opportunity to develop our talents.\"\n\t\t\t\t\t\t-A.P.J. Abdul Kalam");
+	return 0;
+}
